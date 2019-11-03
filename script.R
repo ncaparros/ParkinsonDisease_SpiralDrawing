c1694d16f575c5fffd31651f91bd46ee6c8cfc82
ncaparros <nina.caparros@protonmail.com>
2019-11-03 19:21:30 +0100

    Generate image of drawings

--- script.R
+++ script.R
@@ -2,11 +2,9 @@
 #Libraries
 #---------------
 
-if(!require(dplyr)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(ids)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(stringr)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(tidyr)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(gdata)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
+if(!require(ids)) install.packages("ids", repos = "http://cran.us.r-project.org")
+if(!require(tidyr)) install.packages("tidyr", repos = "http://cran.us.r-project.org")
 
 #---------------
 #Read data
@@ -44,8 +42,57 @@ pwp <- do.call(rbind, lapply(fileNamesPwp, function(x) {
 
 df <- rbind(controls, pwp)
 
+#---------------
+#Prepare Data
+#---------------
+
 df <- df %>% separate(V1, c("X", "Y", "Z", "Pressure", "GripAngle","Timestamp", "TestID"),";")
 
+df <- df %>% mutate(X = as.numeric(X),
+                    Y = as.numeric(Y),
+                    Z = as.numeric(Y),
+                    Pressure = as.numeric(Pressure),
+                    GripAngle = as.numeric(GripAngle))
 #---------------
-#Read data
+#Visualizing images
 #---------------
+
+Healthies <- df %>% 
+  filter(isPwp == FALSE)
+
+Pwps <- df %>% 
+  filter(isPwp == TRUE)
+
+exampleHealthyStatic <- Healthies %>% 
+  filter(patientID == first(patientID) & 
+           TestID == 0) %>% 
+  select(X, Y, Pressure)
+
+examplePWPStatic <- Pwps %>% 
+  filter(patientID == first(patientID) & 
+           TestID == 0) %>% 
+  select(X, Y, Pressure)
+
+mHealthyStatic <- matrix(0,nrow = 500, ncol = 500)
+
+for(i in seq(1, by=1, length=nrow(exampleHealthyStatic))){
+  
+  mHealthyStatic[exampleHealthyStatic[i,]$X, exampleHealthyStatic[i,]$Y] = exampleHealthyStatic[i,]$Pressure
+}
+
+image(mHealthyStatic)
+
+
+mPwpStatic <- matrix(0,nrow = 500, ncol = 500)
+
+for(i in seq(1, by=1, length=nrow(examplePWPStatic))){
+  
+  mPwpStatic[examplePWPStatic[i,]$X, examplePWPStatic[i,]$Y] = examplePWPStatic[i,]$Pressure
+}
+
+image(mPwpStatic)
+#---------------
+#Create training and validation sets
+#---------------
+
+
